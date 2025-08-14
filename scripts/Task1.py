87957f363c9ef2d59717e0053f6628e6d97c7fe5
dvsk <129475232+dvskabangira@users.noreply.github.com>
2025-08-14 10:11:56 +0000

    commit

--- scripts/Task1.py
+++ scripts/Task1.py
@@ -1,5 +1,5 @@
 
-# Dataset (Recommended): Student Performance Factors (Kaggle)
+# Student Performance Factors (Kaggle Dataset)
 # Build a model to predict students' exam scores based on their study hour
 # Perform data cleaning and basic visualization to understand the datase
 # Split the dataset into training and testing set
@@ -14,14 +14,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 
 
-#load data
-data = pd.read_csv("/workspaces/Elevvo_Task1-Student_score_predicition/Data/StudentPerformanceFactors.csv")
-#print(data.head(5))
-#print(data.describe())
+#load data & simple visualization
+data = pd.read_csv("/workspaces/Elvvo_Task1-Student_score_predicition/Data/StudentPerformanceFactors.csv")
+print(data.head(5))
+print(data.describe())
 
 #Data cleaning
 
-#print(data.isnull().sum())
+print(data.isnull().sum())
 df = data.dropna(subset=['Teacher_Quality','Parental_Education_Level','Distance_from_Home'])
 
 #Data preprocessing
@@ -46,17 +46,8 @@ print("Predicted scores:", y_pred)
 # Model performance
 print("Slope (m):", model.coef_[0])
 print("Intercept (c):", model.intercept_)
-print("R² Score:", r2_score(y, y_pred))
-print("RMSE:", np.sqrt(mean_squared_error(y, y_pred)))
-
-# Plot results
-plt.scatter(X, y, color='blue', label='Actual Data')
-plt.plot(X, y_pred, color='red', linewidth=2, label='Regression Line')
-plt.xlabel("Study Hours")
-plt.ylabel("Exam Score")
-plt.title("Study Hours vs Exam Score Prediction")
-plt.legend()
-plt.show()
+print("R² Score:", r2_score(y_test, y_pred))
+print("RMSE:", np.sqrt(mean_squared_error(y_test, y_pred)))
 
 # Predict for a new student
 new_hours = np.array([[7.5]])
